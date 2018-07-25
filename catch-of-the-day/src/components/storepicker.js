a45cf6eed0b7f2251386175bcbcdfd6205360d21
Cassy Myers <cassymyers@gmail.com>
2018-07-24 17:59:25 -0700

    learning push states

--- catch-of-the-day/src/components/storepicker.js
+++ catch-of-the-day/src/components/storepicker.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this.myInput.value.value)
+
+  }
   render() {
     return (
       <React.Fragment>
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore} >
       { /* I'm doin' some Javascript. This is a React.JS comment */ }
       <h2>Please enter a store:</h2>
-      <input type="text" required placeholder="Store Name" />
-      <button tupe="submit">Visit Da Store</button>
+      <input
+        type="text"
+        required
+        placeholder="Store Name"
+        defaultValue={ getFunName() }
+        ref={this.myInput} />
+      <button type="submit">Visit Da Store</button>
       </form>
       </React.Fragment>
       )
